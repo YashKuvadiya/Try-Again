3d5dcf24b9bb71b80925b1b85f710d1b953a2e8c
user <0914yash0914@gmail.com>
2024-08-29 17:48:26 +0530

    init Commit

---
+++ ImagePlace/src/main/java/com/practice/code/imageplace/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.practice.code.imageplace;
+
+import android.util.Log;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+    }
+    public static void printLog(String tag, String message) {
+        Log.e(tag, message);
+    }
+}
---
+++ app/src/main/java/com/practice/code/tryagain/MainActivity.java
@@ -0,0 +1,18 @@
+package com.practice.code.tryagain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.practice.code.imageplace.HelloWorld;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        HelloWorld.printLog("Yash","Kuvadiya");
+    }
+}
